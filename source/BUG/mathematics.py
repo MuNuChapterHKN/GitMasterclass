2f3e39144704ef0f70520b3ecc7dcc030747bb62
scyrock <ciccio.cerrito@gmail.com>
2023-12-13 19:26:24 +0100

    Bug 0012 solved

--- source/BUG/mathematics.py
+++ source/BUG/mathematics.py
@@ -97,7 +97,7 @@ def sin_value(a: float) -> float:
     '''
     BUG012: sin_value should return the sine of the value, not the cosine.
     '''
-    result = math.cos(a)
+    result = math.sin(a)
     return result
 
 
